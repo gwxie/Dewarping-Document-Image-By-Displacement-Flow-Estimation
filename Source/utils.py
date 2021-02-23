d8bf6554576a8c83323403106f45044e0052ccdb
gwxie <74235591+gwxie@users.noreply.github.com>
2021-02-23 15:15:16 +0800

    Update utils.py

--- Source/utils.py
+++ Source/utils.py
@@ -110,7 +110,7 @@ class SaveFlatImage(object):
 
         flat_img = np.full_like(perturbed_img, 256, dtype=np.uint16)
 
-        ''''''
+        '''remove the background of input image(perturbed_img) and forward mapping(perturbed_label)'''
         origin_pixel_position = np.argwhere(np.zeros(flat_shape, dtype=np.uint32) == 0).reshape(flat_shape[0] * flat_shape[1], 2)
         perturbed_label = perturbed_label.reshape(flat_shape[0] * flat_shape[1], 2)
         perturbed_img = perturbed_img.reshape(flat_shape[0] * flat_shape[1], 3)
@@ -118,7 +118,7 @@ class SaveFlatImage(object):
         perturbed_label[perturbed_label_classify != 0, :] += origin_pixel_position[perturbed_label_classify != 0, :]
         pixel_position = perturbed_label[perturbed_label_classify != 0, :]
         pixel = perturbed_img[perturbed_label_classify != 0, :]
-
+        '''construct Delaunay triangulations in all scattered pixels and then using interpolation'''
         vtx, wts = self.interp_weights(pixel_position, origin_pixel_position)
         # wts[np.abs(wts)>=1]=0
         wts_sum = np.abs(wts).sum(-1)
