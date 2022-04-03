7abf5500ab034472c46845a3e67379691af24e2d
gwxie <74235591+gwxie@users.noreply.github.com>
2022-04-03 16:59:05 +0800

    Update test.py

--- Source/test.py
+++ Source/test.py
@@ -4,12 +4,15 @@ guowang xie
 '''
 import sys, os
 import time
-
 import argparse
 import torch
 import re
-
 import warnings
+from pathlib import Path
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]
+sys.path.append('ROOT')
+
 from network import ResnetDilatedRgressAndClassifyV2v6v4c1GN
 import utils as utils
 
@@ -19,24 +22,17 @@ def train(args):
     global _re_date
     if args.resume is not None:
         re_date = re.compile(r'\d{4}-\d{1,2}-\d{1,2}')
-        _re_date = re_date.search(args.resume).group(0)
+        _re_date = re_date.search(args.resume.name).group(0)
         reslut_file = open(path + '/' + date + date_time + ' @' + _re_date + '_' + args.arch + '.log', 'w')
     else:
         _re_date = None
         reslut_file = open(path+'/'+date+date_time+'_'+args.arch+'.log', 'w')
 
     # Setup Dataloader
-    data_split = 'data1024_greyV2'
-    data_path = './dataset/unwarp_new/train/'
-    data_path_validate = './dataset/unwarp_new/train/'+data_split+'/'
-    data_path_test = args.data_path_test
 
-    args.arch = 'flat_img_classifyAndRegress_grey'
-    args.dataset = data_split
+    data_path_test = str(args.data_path_test)+'/'
+
     print(args)
-    print(args, file=reslut_file)
-    print('data_split :' + data_split)
-    print('data_split :' + data_split, file=reslut_file)
 
     n_classes = 2
 
@@ -60,17 +56,17 @@ def train(args):
     if args.resume is not None:
         if os.path.isfile(args.resume):
             print("Loading model and optimizer from checkpoint '{}'".format(args.resume))
-            checkpoint = torch.load(args.resume)
+            checkpoint = torch.load(args.resume, map_location='cuda:'+str(args.gpu))
             model.load_state_dict(checkpoint['model_state'])
             print("Loaded checkpoint '{}' (epoch {})"
                   .format(args.resume, checkpoint['epoch']))
         else:
             print("No checkpoint found at '{}'".format(args.resume))
 
-    FlatImg = utils.FlatImg(args=args, path=path, date=date, date_time=date_time, _re_date=_re_date, data_split=data_split, model=model, \
+    FlatImg = utils.FlatImg(args=args, path=path, date=date, date_time=date_time, _re_date=_re_date, data_split=None, model=model, \
                             reslut_file=reslut_file, n_classes=n_classes, optimizer=None, \
                             loss_fn=None, loss_classify_fn=None, data_loader=PerturbedDatastsForRegressAndClassify_pickle_color_v2C1, data_loader_hdf5=None, \
-                            data_path=data_path, data_path_validate=data_path_validate, data_path_test=data_path_test, data_preproccess=False)          # , valloaderSet=valloaderSet, v_loaderSet=v_loaderSet
+                            data_path=None, data_path_validate=None, data_path_test=data_path_test, data_preproccess=False)          # , valloaderSet=valloaderSet, v_loaderSet=v_loaderSet
     ''' load data '''
     FlatImg.loadTestData()
 
@@ -94,7 +90,7 @@ def train(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Hyperparams')
-    parser.add_argument('--arch', nargs='?', type=str, default='flat_unet',
+    parser.add_argument('--arch', nargs='?', type=str, default='Dewarping-Document-Image-By-Displacement-Flow-Estimation',
                         help='Architecture to use [\'fcn8s, unet, segnet etc\']')
 
     parser.add_argument('--dataset', nargs='?', type=str, default='v5',
@@ -103,24 +99,24 @@ if __name__ == '__main__':
     parser.add_argument('--l_rate', nargs='?', type=float, default=0.0002,
                         help='Learning Rate')
 
-    parser.add_argument('--resume', nargs='?', type=str, default=None,
+    parser.add_argument('--resume', default=ROOT / '2019-06-25 11:52:54/49/2019-06-25 11:52:54flat_img_classifyAndRegress_grey-data1024_greyV2.pkl', type=str, 
                         help='Path to previous saved model to restart from') 
     
-    parser.add_argument('--data_path_test', default='./dataset/shrink_1024_960/crop/', type=str,
+    parser.add_argument('--data_path_test', default=ROOT / 'dataset/shrink_1024_960/crop/', type=str,
                         help='the path of test images.')  # test image path
 
-    parser.add_argument('--output-path', default='./flat/', type=str,
+    parser.add_argument('--output-path', default=ROOT / 'flat/', type=str,
                         help='the path is used to  save output --img or result.')  # GPU id ---choose the GPU id that will be used
 
-    parser.add_argument('--batch_size', nargs='?', type=int, default=6,
+    parser.add_argument('--batch_size', nargs='?', type=int, default=1,
                         help='Batch Size')#16
 
     parser.add_argument('--schema', type=str, default='test',
                         help='train or test')
 
-    parser.set_defaults(resume='./2019-06-25 11:52:54/49/2019-06-25 11:52:54flat_img_classifyAndRegress_grey-data1024_greyV2.pkl')
+    # parser.set_defaults(resume='2019-06-25 11:52:54/49/2019-06-25 11:52:54flat_img_classifyAndRegress_grey-data1024_greyV2.pkl')
 
-    parser.add_argument('--parallel', default='3', type=list,
+    parser.add_argument('--parallel', default='6', type=list,
                         help='choice the gpu id for parallel ')
 
     args = parser.parse_args()
